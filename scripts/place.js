671f14b6701df0b996883186fd7da03e78e3bd1a
georson00 <geo20010@byui.edu>
2024-10-19 15:39:05 -0400

    changing the js

--- scripts/place.js
+++ scripts/place.js
@@ -1,14 +1,11 @@
 const temperature = 31;
 const windSpeed = 8;
+document.querySelector('#wind-chill').textContent = 'N/A'
 
 const calculateWindChill = (temp, speed) =>{
     const windChill = 13.12 + 0.6215 * temp - 11.37 * speed** 0.16 + 0.3965 * temp * speed ** 0.16
-    if( temp <= 10 ){
         return windChill.toFixed(2);
-
-    }
-    else{
-        return 'N/A'
-    }
+    
 }
-document.querySelector('#wind-chill').textContent = calculateWindChill(temperature, windSpeed);
+if( temperature <= 10){
+document.querySelector('#wind-chill').textContent = calculateWindChill(temperature, windSpeed);}
